d3ec2a3ec12dfc30111847be5ce0528824a0c72e
Umesh <ukhunt@ebay.com>
2022-04-14 10:55:58 -0700

    updating from macbook

--- src/main/java/programcreek/arraystring/simplepointer/ShortestWordDistance.java
+++ src/main/java/programcreek/arraystring/simplepointer/ShortestWordDistance.java
@@ -1,6 +1,6 @@
 package programcreek.arraystring.simplepointer;
 
-import org.apache.poi.util.SystemOutLogger;
+//import org.apache.poi.util.SystemOutLogger;
 
 public class ShortestWordDistance {
 /*Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
